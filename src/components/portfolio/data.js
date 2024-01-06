112045b81b0f3c04aeb90f08e0caf7768a9558fc
Cristiano Vieira <cristianovieirati@gmail.com>
2024-01-06 18:57:20 -0300

    atualizacao

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -3,6 +3,7 @@ import Colombo from "../../assets/colombo.png";
 import FBV from "../../assets/fbv.png";
 import Gazin from "../../assets/gazin.png";
 import House from "../../assets/house.png";
+import PDC from "../../assets/pdc.png";
 import Redes from "../../assets/redes.png";
 import Serede from "../../assets/serede.png";
 import Siga from "../../assets/siga.png";
@@ -73,12 +74,22 @@ export const portData = [
     buttonTwo: "Website",
   },
   {
-    id: 7,
+    id: 8,
     img: FBV,
     title: "FBV 2023",
     github:
       "https://play.google.com/store/apps/details?id=br.com.feirabrasileiradovarejo.app",
     demo: "https://apps.apple.com/us/app/fbv-2023/id6448462315",
+    buttonOne: "Google Play",
+    buttonTwo: "App Store",
+  },
+  {
+    id: 9,
+    img: PDC,
+    title: "Parceiro da Construção",
+    github:
+      "https://apps.apple.com/br/app/parceiro-da-constru%C3%A7%C3%A3o/id1567978563",
+    demo: "https://play.google.com/store/apps/details?id=com.parceirodaconstrucao",
     buttonOne: "Google Play",
     buttonTwo: "App Store",
   },
