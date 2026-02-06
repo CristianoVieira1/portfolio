317d123065f84a74f2aca126f130000e6aca3d65
CristianoVieira1 <cristianovieirati@gmail.com>
2026-02-06 15:15:17 -0300

    Updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,11 +7,6 @@ $(function () {
   const imgLoad = imagesLoaded(content)
 
   imgLoad.on('done', (instance) => {
-    document.getElementById('loaderContent').classList.add('fade-out')
-    setTimeout(() => {
-      document.getElementById('loader').classList.add('loaded')
-    }, 300)
-
     gsap.set('.animate-headline', { y: 50, opacity: 0 })
     ScrollTrigger.batch('.animate-headline', {
       interval: 0.1,
